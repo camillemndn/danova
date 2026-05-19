bfbebffb2b4019ab7624e6aa58382d551a02d6c9
Camille M. (genesis) <camillemondon@free.fr>
2026-05-19 02:18:08 +0200

    feat(figures): wider

--- setup.R
+++ setup.R
@@ -1,20 +1,6 @@
 library(ggplot2)
 
-theme_set(theme_minimal(base_size = 16))
-theme_legend_inside <- theme(
-	legend.position = "inside",
-	legend.position.inside = c(.05, .25),
-	legend.justification = c("left", "bottom"),
-	legend.box.just = "left",
-	legend.margin = margin(6, 6, 6, 6)
-)
-theme_legend_inside_right <- theme(
-	legend.position = "inside",
-	legend.position.inside = c(.95, .95),
-	legend.justification = c("right", "top"),
-	legend.box.just = "right",
-	legend.margin = margin(6, 6, 6, 6)
-)
+theme_set(theme_minimal(base_size = 12))
 
 stars_pval <- function(x) {
 	stars <- c("***", "**", "*", NULL)
